ca14e78587b8c4b7edfe68cd1307b68cc01de505
= <tsmit291@icloud.com>
2016-04-03 14:38:40 -0600

    global install of yo, bower and grunt

--- ang_app/client/app/admin/admin.module.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('angAppApp.admin', [
-  'angAppApp.auth',
-  'ngRoute'
-]);
--- ang_app/client/app/main/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('angAppApp')
-  .config(function($routeProvider) {
-    $routeProvider
-      .when('/', {
-        template: '<main></main>'
-      });
-  });
