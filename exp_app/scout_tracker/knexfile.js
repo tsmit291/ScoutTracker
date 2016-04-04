3fe72f975e639f286562c82a97ad0bbd4d6a665f
= <tsmit291@icloud.com>
2016-04-04 10:12:15 -0600

    added config settings in knexfile.js

---
+++ exp_app/scout_tracker/knexfile.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/tracker'
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + '?ssl=true'
+  }
+
+};
