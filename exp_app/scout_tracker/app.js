4c8a4dfb1fd381f8f1908243fc5ecfe32d8c1c0f
= <tsmit291@icloud.com>
2016-04-04 10:16:31 -0600

    require knex in index routes file

--- exp_app/scout_tracker/app.js
+++ exp_app/scout_tracker/app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var env = require('dotenv').load();
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/admin', admin);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
