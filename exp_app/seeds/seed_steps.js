1c912428ccaa72fc56fcdcfa56bb85e2981698e1
= <tsmit291@icloud.com>
2016-04-04 13:37:25 -0600

    daisy light blue patch

--- exp_app/seeds/seed_steps.js
+++ exp_app/seeds/seed_steps.js
@@ -8,17 +8,22 @@ exports.seed = function(knex, Promise) {
     knex('steps').insert({
       badge_id: '1',
       step_description: 'Share with a group why you feel it is important to be honest.',
-      step_weight: '0'
+      step_weight: '1'
     }),
     knex('steps').insert({
       badge_id: '1',
       step_description: 'Read the story of The Boy Who Cried Wolf. Discuss the boy. Was he honest? How could he have done things differently?',
-      step_weight: '2'
+      step_weight: '3'
     }),
     knex('steps').insert({
       badge_id: '1',
       step_description: 'Share with a group what you think being fair means. Can you describe a time where you felt that something was not fair? How did you react?',
       step_weight: '2'
+    }),
+    knex('steps').insert({
+      badge_id: '1',
+      step_description: 'Take the time to complete the I Share! Daisy coloring page. Can you describe what you see in the picture?',
+      step_weight: '1'
     }),
   );
 };
