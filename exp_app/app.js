60a03803f9fb425557b6b03d73f221470d1695c3
= <tsmit291@icloud.com>
2016-04-04 11:06:09 -0600

    express server working locally

--- exp_app/app.js
+++ exp_app/app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var env = require('dotenv').load();
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var users = require('./routes/admin');
 
 var app = express();
 
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/admin', admin);
+app.use('/admin', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
