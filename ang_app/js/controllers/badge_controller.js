7ac076e73c3fef082747c4c03606ef7553111354
= <tsmit291@icloud.com>
2016-04-08 14:26:23 -0600

    badge controller

--- ang_app/js/controllers/badge_controller.js
+++ ang_app/js/controllers/badge_controller.js
@@ -1,7 +1,7 @@
 app.controller('badgeController', function($scope, $routeParams, oneBadgeService){
-  id = $routeParams.id;
+  badge_id = $routeParams.id;
 
-  oneBadgeService.getBadge(id).then(function(payload){
+  oneBadgeService.getBadge(badge_id).then(function(payload){
     $scope.oneBadge = payload.data[0];
   }, function(error){
     console.log("an error has occured");
