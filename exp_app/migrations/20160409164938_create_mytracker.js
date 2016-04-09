48aec669161cceaea3e2f4fc0d6452d3e1e9bd93
= <tsmit291@icloud.com>
2016-04-09 16:54:28 -0600

    knex migrations work

--- exp_app/migrations/20160409164938_create_mytracker.js
+++ exp_app/migrations/20160409164938_create_mytracker.js
@@ -1,8 +1,15 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('mytracker', function(table){
+    table.increments();
+    table.integer('contact_id');
+    table.integer('badge_id');
+    table.string('badge_image');
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.boolean('backlog_status');
+    table.boolean('current_status');
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('mytracker');
 };
