b30df029a6682070f9cb8ca2e312c29945d375ee
= <tsmit291@icloud.com>
2016-04-10 12:07:33 -0600

    you can now see badge and steps on the tracker page

--- ang_app/js/controllers/tracker_controller.js
+++ ang_app/js/controllers/tracker_controller.js
@@ -3,8 +3,8 @@ app.controller('trackerController', function ($scope, $routeParams, addBadgeServ
   $scope.currentBadge = $routeParams.badge_id;
   addBadgeService.getBadge($scope.currentScout, $scope.currentBadge).then(function(results){
     $scope.badgey = results.data.result[0]
-    $scope.steps = results.data.rows[1]
-    console.log(results);
+    $scope.steps = results.data.rows
+    console.log(results.data.rows);
   }), function(error){
     console.log("an error has occured");
   };
