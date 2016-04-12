52ebf83daab7f4c5c47746eefd06963787475adf
= <tsmit291@icloud.com>
2016-04-11 20:51:16 -0600

    it is all showing on this page

--- ang_app/js/controllers/tracker_controller.js
+++ ang_app/js/controllers/tracker_controller.js
@@ -2,13 +2,14 @@ app.controller('trackerController', function ($scope, $routeParams, addBadgeServ
   $scope.currentScout = $routeParams.id;
   $scope.currentBadge = $routeParams.badge_id;
    addBadgeService.getBadge($scope.currentScout, $scope.currentBadge).then(function(badges){
-     $scope.badgeCollection = badges;
-     for (var i in $scope.badgeCollection) {
-       if ($scope.badgeCollection[i].badge_id === $scope.currentBadge) {
+     $scope.badgeCollection = badges.data;
+     for (var i=0;i<$scope.badgeCollection.length;i++) {
+       if ($scope.badgeCollection[i].badge_id === parseInt($scope.currentBadge)) {
          $scope.steps = $scope.badgeCollection[i].steps;
          $scope.badgey = $scope.badgeCollection[i];
          break;
        }
      }
+     console.log($scope.steps);
    });
 });
